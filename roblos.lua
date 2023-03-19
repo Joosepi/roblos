f89ac787313a2c7396afa46f56711bc91afdb672
Joosepi <martinjoosep.reiljan@ametikool.ee>
2023-03-19 14:43:37 -0700

    changes

--- roblos.lua
+++ roblos.lua
@@ -1,68 +1,57 @@
 local hotkey = "t" -- toggle key
 local mouse = game.Players.LocalPlayer:GetMouse()
 
-mouse.KeyDown:Connect(function(key)
-    if key == hotkey then
-        getgenv().ValiantAimHacks.SilentAimEnabled = not getgenv().ValiantAimHacks.SilentAimEnabled
-    end
-end)
-
--- Services
+-- // Services
 local Players = game:GetService("Players")
 
--- Variables
+-- // Vars
 local LocalPlayer = Players.LocalPlayer
-local accommodationFactor = 0.12235
+local SilentAimEnabled = false
+local TargetPart = "UpperTorso", "HumanoidRootPart", "Head",
+local AccommodationFactor = 0.143324
+local FOV = 24
 
--- Silent Aim Module
+-- // Silent Aim Module
 local SilentAim = loadstring(game:HttpGet("https://pastebin.com/raw/2f0mGbMP"))()
 SilentAim.TeamCheck = false
 
--- Hook function
-local hookFunction = function(t, k)
-    if t:IsA("Mouse") and k == "Hit" then
-        local selectedPlayer = SilentAim.Selected
-        if SilentAim.SilentAimEnabled and selectedPlayer ~= LocalPlayer and not selectedPlayer.Character.BodyEffects["K.O"].Value 
-            and not selectedPlayer.Character:FindFirstChild("GRABBING_CONSTRAINT") then
-            local parts = {...}
-            for i, partName in ipairs(parts) do
-                local part = selectedPlayer.Character:FindFirstChild(partName)
-                if part then
-                    local velocity = partName == "Humanoid" and part.RootPart.Velocity or part.Velocity
-                    return {p = part.CFrame.p + velocity * accommodationFactor}
-                end
-            end
-        end
-    end
-    return mt.__index(t, k)
+-- // Check if player is down
+local function checkSilentAim()
+    local checkA = SilentAimEnabled and SilentAim.Selected ~= LocalPlayer
+    local playerCharacter = SilentAim.Selected.Character
+    local daHood = (playerCharacter.BodyEffects["K.O"].Value == false or playerCharacter:FindFirstChild("GRABBING_CONSTRAINT") ~= nil)
+    return checkA and daHood
 end
 
--- Metatable vars
+-- // Hook
 local mt = getrawmetatable(game)
 local backupIndex = mt.__index
 setreadonly(mt, false)
-mt.__index = newcclosure(hookFunction)
-setreadonly(mt, true)
 
--- Set the FOV
-getgenv().ValiantAimHacks.FOV = 24
-
-
--- Hook
-local hookFunction = function(t, k)
+mt.__index = newcclosure(function(t, k)
     if t:IsA("Mouse") and k == "Hit" then
-        local selectedPlayer = SilentAim.Selected
-        if SilentAim.SilentAimEnabled and selectedPlayer ~= LocalPlayer and not selectedPlayer.Character.BodyEffects["K.O"].Value 
-            and not selectedPlayer.Character:FindFirstChild("GRABBING_CONSTRAINT") then
-            local parts = {"UpperTorso", "HumanoidRootPart"}
-            for i, partName in ipairs(parts) do
-                local part = selectedPlayer.Character:FindFirstChild(partName)
-                if part then
-                    local velocity = partName == "Humanoid" and part.RootPart.Velocity or part.Velocity
-                    return {p = part.CFrame.p + velocity * accommodationFactor}
-                end
+        local CPlayer = SilentAim.Selected
+        if checkSilentAim() and CPlayer.Character and CPlayer.Character:FindFirstChild(TargetPart) then
+            local TargetCFrame = CPlayer.Character[TargetPart].CFrame
+            local TargetVelocity = CPlayer.Character[TargetPart].Velocity
+            local AimPosition = TargetCFrame.p + TargetVelocity * AccommodationFactor
+            local CurrentPosition = LocalPlayer.Character.Head.Position
+            local Distance = (AimPosition - CurrentPosition).magnitude
+
+            if Distance <= FOV then
+                return {p = AimPosition}
             end
         end
     end
-    return mt.__index(t, k)
-end
+    return backupIndex(t, k)
+end)
+
+-- // Revert
+setreadonly(mt, true)
+
+-- // Toggle Silent Aim
+mouse.KeyDown:Connect(function(key)
+    if key == hotkey then
+        SilentAimEnabled = not SilentAimEnabled
+    end
+end)
